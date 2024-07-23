eb73ce2a459c50f89d603ca4b0814cffe23b9307
Conor Callahan <conorcallahan97@outlook.com>
2024-07-23 16:49:07 -0500

    Created the Movie details component and its nesting folder. I also implemented a GET route to fetch the required genres for the Movie details page.

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,16 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
+  // Added query to get all genres, 
+  // to be used to display the genre of a selected movie poster.
+  const queryText = 'SELECT * FROM genres ORDER BY name ASC';
+  pool.query(queryText)
+  .then(result => {
+    res.json(res.rows);
+  }).catch(error => {
+    console.error('Error fetching genres', error);
   res.sendStatus(500)
+  });
 });
 
 module.exports = router;
